d1c0aa397f36498b4fe673d45f5ddb402d21426b
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2026-01-06 10:34:35 +0100

    Create WhenSharedKillSignalsOfMessagesByHalfSignalsAlgorithm.h

---
+++ WhenSharedKillSignalsOfMessagesByHalfSignalsAlgorithm.h
@@ -0,0 +1,10 @@
+//We can promote UI/UX signals in a purely chip. To avoid shutdown of
+//ther signals that help messages between middlewares, we can kill a lot of internal signals of a messaging system including many programs.
+//Then, we need a scheduller that must be suited to high range noises in messages patterns. To attack this lat system, we need to promote a second thread above the principal thread
+//of middlewares tools communicating (not process).
+
+class avoidSignalsAtTheEndOfScopeByNewThreadsAndKillSecondPureThreadRecognizedByScopeItself
+{
+
+
+}
